ace949b9a962ff6b4086a73544d0810251ed1ca3
radek_random <12987981+radekxrandom@users.noreply.github.com>
2023-01-19 16:06:02 +0000

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,15 @@
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { Module } from '@nestjs/common';
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import { Module } from "@nestjs/common";
 
-import { User } from './models/user.model';
-import { UsersController } from './http/controllers/users.controller';
-import { UsersFactory } from './factories/users.factory';
-import { UsersService } from './services/users.service';
+import { User } from "./models/user.model";
+import { UsersController } from "./http/controllers/users.controller";
+import { UserFactory } from "./factories/user.factory";
+import { UsersService } from "./services/users.service";
+import { UserCreatedRedisListener } from "./redist-listeners/user-created.redis-listener";
 
 @Module({
   imports: [MikroOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, UsersFactory],
+  providers: [UsersService, UserFactory, UserCreatedRedisListener],
 })
 export class UsersModule {}
